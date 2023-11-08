d1f36f5bf4f2f3327fbc2494deae1c1f03971cbf
Anders Sundin <olsu2201@student.miun.se>
2023-11-08 08:53:41 +0100

    Kod för att lägga till poster

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 //base url
 const url = 'http://localhost:3000/courses';
 let tableData = document.getElementById('table-data');
-
+const code = document.getElementById('code');
+const courseName = document.getElementById('name');
+const progression = document.getElementById('progression');
+const term = document.getElementById('term');
+const syllabus = document.getElementById('syllabus');
 async function fetchData() {
     try {
         const response = await fetch(url);
@@ -34,7 +38,7 @@ async function fetchData() {
 }
 
 async function deleteCourse(id) {
-    console.log(id);
+    
     const deleteURL = `http://localhost:3000/courses/${id}`;
 
     try {
@@ -55,3 +59,44 @@ async function deleteCourse(id) {
 
 // Anropa funktionen för att hämta data
 fetchData();
+
+
+async function addCourse(e) {
+    e.preventDefault();
+    let data = {
+        code: code.value,
+        name: courseName.value,
+        progression: progression.value,
+        term: term.value,
+        syllabus: syllabus.value
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            // get all courses
+            fetchData();
+            // empty inputfields
+            code.value = '';
+            courseName.value = '';
+            progression.value = '';
+            term.value = '';
+            syllabus.value = '';
+
+        } else {
+            throw new Error('Något gick fel då kurs skulle läggas till.');
+        }
+    } catch (error) {
+        console.error('Ett fel uppstod:', error);
+    }
+
+}
+const addCourseForm = document.getElementById('add-course-form');
+addCourseForm.addEventListener('submit', addCourse);
